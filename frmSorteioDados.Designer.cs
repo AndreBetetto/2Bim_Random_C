88c5d471834c494775f6819349d73ab9770772e2
ElitePower27 <andre.betetto@unesp.br>
2022-05-02 10:37:12 -0300

    frmSorteioDados, Design

--- frmSorteioDados.Designer.cs
+++ frmSorteioDados.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picBoxQuadradro1 = new System.Windows.Forms.PictureBox();
+            this.picBoxQuadradro2 = new System.Windows.Forms.PictureBox();
+            this.lblPlacarJog1 = new System.Windows.Forms.Label();
+            this.lblPlacarJog2 = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.txtboxNickName1 = new System.Windows.Forms.TextBox();
+            this.txtboxNickName2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxQuadradro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxQuadradro2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxQuadradro1
+            // 
+            this.picBoxQuadradro1.Location = new System.Drawing.Point(88, 37);
+            this.picBoxQuadradro1.Name = "picBoxQuadradro1";
+            this.picBoxQuadradro1.Size = new System.Drawing.Size(147, 142);
+            this.picBoxQuadradro1.TabIndex = 0;
+            this.picBoxQuadradro1.TabStop = false;
+            // 
+            // picBoxQuadradro2
+            // 
+            this.picBoxQuadradro2.Location = new System.Drawing.Point(308, 37);
+            this.picBoxQuadradro2.Name = "picBoxQuadradro2";
+            this.picBoxQuadradro2.Size = new System.Drawing.Size(147, 142);
+            this.picBoxQuadradro2.TabIndex = 1;
+            this.picBoxQuadradro2.TabStop = false;
+            // 
+            // lblPlacarJog1
+            // 
+            this.lblPlacarJog1.BackColor = System.Drawing.Color.LightGray;
+            this.lblPlacarJog1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPlacarJog1.Location = new System.Drawing.Point(88, 198);
+            this.lblPlacarJog1.Name = "lblPlacarJog1";
+            this.lblPlacarJog1.Size = new System.Drawing.Size(147, 23);
+            this.lblPlacarJog1.TabIndex = 2;
+            // 
+            // lblPlacarJog2
+            // 
+            this.lblPlacarJog2.BackColor = System.Drawing.Color.LightGray;
+            this.lblPlacarJog2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPlacarJog2.Location = new System.Drawing.Point(305, 198);
+            this.lblPlacarJog2.Name = "lblPlacarJog2";
+            this.lblPlacarJog2.Size = new System.Drawing.Size(150, 23);
+            this.lblPlacarJog2.TabIndex = 3;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(227, 254);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(79, 23);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Jogar";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // txtboxNickName1
+            // 
+            this.txtboxNickName1.Location = new System.Drawing.Point(88, 11);
+            this.txtboxNickName1.Name = "txtboxNickName1";
+            this.txtboxNickName1.Size = new System.Drawing.Size(147, 20);
+            this.txtboxNickName1.TabIndex = 5;
+            // 
+            // txtboxNickName2
+            // 
+            this.txtboxNickName2.Location = new System.Drawing.Point(308, 11);
+            this.txtboxNickName2.Name = "txtboxNickName2";
+            this.txtboxNickName2.Size = new System.Drawing.Size(147, 20);
+            this.txtboxNickName2.TabIndex = 6;
+            // 
+            // frmSorteioDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtboxNickName2);
+            this.Controls.Add(this.txtboxNickName1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblPlacarJog2);
+            this.Controls.Add(this.lblPlacarJog1);
+            this.Controls.Add(this.picBoxQuadradro2);
+            this.Controls.Add(this.picBoxQuadradro1);
+            this.Name = "frmSorteioDados";
             this.Text = "SorteioDados";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxQuadradro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxQuadradro2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picBoxQuadradro1;
+        private System.Windows.Forms.PictureBox picBoxQuadradro2;
+        private System.Windows.Forms.Label lblPlacarJog1;
+        private System.Windows.Forms.Label lblPlacarJog2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtboxNickName1;
+        private System.Windows.Forms.TextBox txtboxNickName2;
     }
 }
